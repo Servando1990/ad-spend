7d0cad6f2d37aabf1828ed3fecd7fc4f8aef677d
Servando <vandotorresg@gmail.com>
2025-03-19 11:41:45 -0600

    Update: Next.js configuration for production build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig 
